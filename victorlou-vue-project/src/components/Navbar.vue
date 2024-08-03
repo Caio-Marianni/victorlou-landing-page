a0730587eec026ecacc3263522eed19d1e2856f1
Caio <caiomarianni@gmail.com>
2024-08-03 17:56:33 -0300

    Working on audio playback, now it's time to work on the style

--- victorlou-vue-project/src/components/Navbar.vue
+++ victorlou-vue-project/src/components/Navbar.vue
@@ -1,16 +1,14 @@
 <script setup>
-import Navigation from "./Navigation.vue";
 import IconLogo from "@/assets/icons/IconLogo.vue";
-import NavigationMobile from "./NavigationMobile.vue";
 import IconLogoName from "@/assets/icons/IconLogoName.vue";
-import MusicCards from "./MusicCard.vue";
- </script>
+import Navigation from "./Navigation.vue";
+import NavigationMobile from "./NavigationMobile.vue";
+</script>
 <template>
   <div class="flex flex-col justify-between">
-    <MusicCards /> 
     <IconLogo class="w-10" />
     <IconLogoName class="w-40" />
-    <Navigation />
+    <!-- <Navigation /> -->
     <NavigationMobile />
   </div>
 </template>
--- victorlou-vue-project/src/components/Releases.vue
+++ victorlou-vue-project/src/components/Releases.vue
@@ -1,6 +1,40 @@
-<script setup>
-import MusicCard from './(SemUso)MusicCards.vue';
+<script>
+import MusicCard from "./MusicCard.vue";
+
+export default {
+  components: { MusicCard },
+  data() {
+    return {
+      tracks: [
+        {
+          title: "Untitled (Trance Remix)",
+          artist: "Victor Lou, Mazuk ",
+          cover: "src/assets/cover/teste.webp",
+          music: "src/assets/music/Untitled(TranceRemix).mp3",
+          linkSpotify:
+            "https://open.spotify.com/intl-pt/album/6n9HAIjtM3wF1AY8QfXv7p",
+        },
+        {
+          title: "Untitled (Trance Remix)",
+          artist: "Victor Lou, Mazuk ",
+          cover: "src/assets/cover/teste.webp",
+          music: "src/assets/music/Untitled.ogg",
+          linkSpotify:
+            "https://open.spotify.com/intl-pt/album/6n9HAIjtM3wF1AY8QfXv7p",
+        },
+      ],
+    };
+  },
+};
 </script>
 <template>
-  <MusicCard />
+  <h1>Releases ON</h1>
+  <div class="flex gap-10 items-center" v-for="track in tracks" :key="track.id">
+    <MusicCard
+      :title="track.title"
+      :artist="track.artist"
+      :cover="track.cover"
+      :music="track.music"
+    />
+  </div>
 </template>
