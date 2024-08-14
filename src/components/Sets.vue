2e00a147e5f375e335a57db82b9cc0f9aa812f96
Caio <caiomarianni@gmail.com>
2024-08-14 01:01:54 -0300

    another try

--- src/components/Sets.vue
+++ src/components/Sets.vue
@@ -37,10 +37,10 @@ export default {
   <div v-for="set in sets" :key="set.id">
     <!-- Card -->
     <div class="group relative h-[300px] text-center shadow-[0px_15px_35px_-5px] border-2 rounded-lg overflow-hidden">
-      <img class="object-cover h-full w-full transition-all group-hover:blur-[4px] lg:-pt-10" :..="set.cover" alt="Set Cover" />
+      <img class="object-cover h-full w-full transition-all group-hover:blur-[4px] lg:-pt-10" :src="set.cover" alt="Set Cover" />
       
       <div class="absolute top-0">
-        <img class="object-cover h-full w-full transition-all lg:-pt-10" :..="set.cover2" alt="Set Cover2" />
+        <img class="object-cover h-full w-full transition-all lg:-pt-10" :src="set.cover2" alt="Set Cover2" />
       </div>
       <!-- info -->
       <div class="absolute py-1 bg-cSixty500 text-[#c8d3ff] font-bold flex flex-col justify-center w-full bottom-[20px] translate-y-4 opacity-0 transition-[0.5s] delay-[calc(0.1s_*_var(--i))] group-hover:translate-y-0 group-hover:opacity-100">
